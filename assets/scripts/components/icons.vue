44a42addc4e77c32db998a17ae5d2c3c72871e81
David Dashifen Kees <dashifen@dashifen.com>
2022-10-13 07:40:44 -0400

    trying to componetize icons

---
+++ assets/scripts/components/icons.vue
@@ -0,0 +1,13 @@
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+export default {
+  name: "FontAwesomeIcon",
+  components: [FontAwesomeIcon],
+  props: ["set", "icon"],
+}
+</script>
+
+<template>
+  <font-awesome-icon :icon="[set, icon]"></font-awesome-icon>
+</template>
--- assets/scripts/icons.js
+++ assets/scripts/icons.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faHouse, faHouseNight } from '@fortawesome/pro-solid-svg-icons';
+import { default as Icons } from './components/icons.vue';
 
 export default {
   initialize() {
     library.add(faHouse, faHouseNight);
-    createApp(FontAwesomeIcon).mount('font-awesome-icon');
+    document.querySelectorAll('icon').forEach((element) => {
+      createApp(Icons).mount(element);
+    });
   }
 }
 
